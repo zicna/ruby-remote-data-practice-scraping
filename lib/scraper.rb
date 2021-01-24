0ccb0f8189650acc049d2142128393ba269251f7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-01-24 12:40:12 -0600

    commit tree 4ec1586d6f9471b2e5ef503e845b0742444cbc8c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/")) 
+
+#doc.css(".site-header__hero__headline").text.strip
+
+courses = doc.css(".tout__label.heading.heading--level-4") 
+
+courses.each do |course|
+    puts course.text.strip
+end
+
+
